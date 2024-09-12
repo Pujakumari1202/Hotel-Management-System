61cab97e52045c1fcdf54ddf6dfc9f5c6748fc5c
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2024-09-12 21:08:46 +0530

    updated

--- src/Hotel/Management/System/Splash.java
+++ src/Hotel/Management/System/Splash.java
@@ -1,4 +1,31 @@
 package Hotel.Management.System;
 
-public class Splash {
+import javax.swing.*;
+
+public class Splash extends JFrame {
+    public Splash()
+    {
+        ImageIcon imageIcon=new ImageIcon(ClassLoader.getSystemResource("icons/splash.gif"));
+        JLabel label=new JLabel(imageIcon);
+        label.setBounds(0,0,858,680);
+        add(label);
+
+
+        setLayout(null);
+        setLocation(300,80);
+        setSize(858,680);
+        setVisible(true);
+
+        try{
+            Thread.sleep(5000);//showing for 5 sec then closed
+            setVisible(false);//using this
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new Splash();
+    }
 }
